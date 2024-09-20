56a031636294eee7bf474b9d38505dba59a7af97
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-09-20 13:37:54 +0200

    done

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,20 +1,71 @@
+import { useState } from "react"
 
-function AddForm() {
+function AddForm(props) {
+
+  const { setAllProducts } = props
+
+  const [ nameValue, setNameValue ] = useState("")
+  const [ priceValue, setPriceValue ] = useState(0)
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    let value = event.target.value
+    // setNameValue(value.toUpperCase() + "TROL")
+    setNameValue(value)
+  }
+
+  const handlePriceChange = (event) => {
+    setPriceValue(event.target.value)
+  }
+
+  const handleAddProduct = (event) => {
+    event.preventDefault() // remueve todos los comportamiento predeterminados de este evento
+    console.log("añadiendo producto")
+
+    // 1. recopilar el producto a crear
+    const newProduct = {
+      name: nameValue,
+      price: priceValue,
+      isPurchased: false // valor inicial que siempre será así al inicio
+    }
+    console.log(newProduct)
+    
+    // 2. agregarlo a donde estan todos los productos
+    // const clone = [...allProducts]
+    // clone.unshift(newProduct)
+    // setAllProducts(clone)
+
+    // a las funciones set les puedo pasar una funcion de callback
+    setAllProducts( (elValorActualDelEstado) => {
+      console.log(elValorActualDelEstado)
+
+      const clone = [...elValorActualDelEstado]
+      clone.unshift(newProduct)
+
+      // retornamos lo que queremos que sea el nuevo valor
+      return clone
+    } )
+
+    // sintaxis muy avanzada de lo mismo que está escrito antes
+    // setAllProducts((current) => [newProduct, ...current])
+
+  }
+
   return (
     <div className="add-form container">
       
       <h2>Add Form</h2>
 
-      <form>
+      <form onSubmit={handleAddProduct}>
 
         <div>
           <label htmlFor="name">Name: </label>
-          <input type="text" name="name"/>
+          <input onChange={handleNameChange} value={nameValue} type="text" name="name"/>
         </div>
 
         <div>
           <label htmlFor="price">Price: </label>
-          <input type="number" name="price"/>
+          <input onChange={handlePriceChange} value={priceValue} type="number" name="price" />
         </div>
 
         <button>Add</button>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
 
-function SearchBar() {
+function SearchBar(props) {
+
+  const { searchValue, setSearchValue } = props
+
+  
+
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value)
+    // ! nunca tenemos acceso al nuevo valor del estado en la misma secuencia de código donde lo estamos actualizando
+    
+    // const filteredProducts = allProducts.filter((eachProduct) => {
+    //   return eachProduct.name.includes(event.target.value)
+    // })
+    
+    // setFilteredProducts(filteredProducts)
+
+  }
+
   return (
     <div className="search-bar container">
       
       <h2>Search Bar</h2>
         
-      <input type="text" />
+      <input onChange={handleSearchChange} value={searchValue} type="text" />
 
     </div>
   )
